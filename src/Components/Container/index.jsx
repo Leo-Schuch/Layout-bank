e88b8c14849a500fb79ab7097c548c784855d458
Leo-Schuch <leoschuch23@gmail.com>
2022-07-30 11:50:04 -0300

    new
    1step

---
+++ src/Components/Container/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import Titulo from "../Titulo";
+import Conta from "../Conta";
+import styled from "styled-components";
+import Extrato from "../Extrato";
+
+const Container = styled.div`
+background-color: ${ ({theme})=> theme.body};
+  min-height: 90vh;
+  padding: 0px 15vw;
+  `
+
+const Conteudo = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+color: ${ ({theme})=> theme.text};
+
+@media (max-width: 800px) {
+  flex-direction: column;
+  }
+`
+
+export default () => {
+  return (
+    <Container>
+      <Titulo>Olá Leonardo</Titulo>
+      <Conteudo>
+        <Conta />
+        <Extrato/>
+      </Conteudo>
+    </Container>
+  );
+};
+
+
